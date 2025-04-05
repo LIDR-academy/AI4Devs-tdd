08c5062569f0a6a322e199170d4437cf38335392
Javi Sanz <javier.sanz@gmail.com>
2025-04-05 17:12:13 +0200

    Update project structure and dependencies. Renamed project to "ai4devs-tdd" with version 1.0.0. Enhanced testing setup by adding Jest configuration for frontend, including test scripts and mock files. Updated backend dependencies with express-validator and nodemon, and added new test scripts for concurrent testing. Added initial test files for frontend components.

---
+++ frontend/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    url: 'http://localhost'
+  },
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+}; 
